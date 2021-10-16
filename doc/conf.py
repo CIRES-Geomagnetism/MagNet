2aaf028facf89c254136a43981ec2d55368e4301
Belinda Trotta <btrotta@users.noreply.github.com>
2021-10-16 12:54:10 +1100

    Update docs

--- doc/conf.py
+++ doc/conf.py
@@ -12,15 +12,16 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'magnet-cnn'
-copyright = '2021, Belinda Trotta'
-author = 'Belinda Trotta'
+project = "magnet-cnn"
+copyright = "2021, Belinda Trotta"
+author = "Belinda Trotta"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,18 +29,15 @@ author = 'Belinda Trotta'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-extensions = ['sphinx.ext.autodoc']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,14 +45,14 @@ extensions = ['sphinx.ext.autodoc']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Latex Options
 
-latex_theme = 'manual'
+latex_theme = "manual"
--- doc/index.rst
+++ doc/index.rst
@@ -3,13 +3,39 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to magnet-cnn's documentation!
-======================================
+Title
+=====
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
+   intro
 
-.. automodule:: nn_functions
+Source code reference
+=====================
+
+preprocessing
+-------------
+.. automodule:: preprocessing
    :members:
+
+model_definitions
+-----------------
+.. automodule:: model_definitions
+   :members:
+
+train
+-----
+.. automodule:: train
+   :members:
+
+predict
+-------
+.. automodule:: predict
+   :members:
+
+
+Scripts
+=======
+
+.. automodule:: train_on_all_data
